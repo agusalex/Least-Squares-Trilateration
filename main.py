38178b63b2b69abd6dde33600f798607f38de2b2
Agustin Alexander <agustin.c.alexander@gmail.com>
2023-06-21 20:03:49 -0300

    added config

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import matplotlib.pyplot as plt
+import json
+
 import numpy as np
 import pandas as pd
 from numpy import ndarray
 from scipy import stats
-
-from easy_trilateration.kalman import KalmanFilter
-from easy_trilateration.model import *
 import argparse
 from easy_trilateration.least_squares import *
 from easy_trilateration.graph import *
@@ -92,7 +90,11 @@ def amountOfDifferentNodes(circles: [Circle]):
 
 
 def distance(row_i):
-    return rssi_to_distance(row_i['rssi']) + float(node_location[row['node']]['offset'])
+    if len(node_location) > 0:
+        n = node_location[row['node']]
+        return rssi_to_distance(row_i['rssi'], int(n["A"]), int(n["N"])) + float(n['offset'])
+    else:
+        return rssi_to_distance(row_i['rssi'])
 
 
 def rssi_to_distance(rssi, a=36, n=27):
@@ -102,12 +104,28 @@ def rssi_to_distance(rssi, a=36, n=27):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Trilateration solver and 2D grapher')  # cuadrado_diagonal_kalman.csv
-    parser.add_argument('--file', nargs='?', help='data filename', default='resources/simulation/capture_combined.csv')
+    parser.add_argument('--file', nargs='?', help='data filename',
+                        default='resources/simulation/capture_combined.csv')
 
     args = parser.parse_args()
 
     _filename = args.file
-
+    path_groups = _filename.split("/")
+    default_simulation = {"actual": [], "min_nodes": 5, "node_location": {
+    }}
+    try:
+        with open("/".join(_filename.split("/")[:len(path_groups) - 1]) + "/locations.json") as json_f:
+            json_file = json.load(json_f)
+            file = path_groups[len(path_groups) - 1]
+            csv_metadata = json_file.get(file, default_simulation)
+            node_location = json_file["node_location"]
+            min_nodes = csv_metadata["min_nodes"]
+            actual = csv_metadata["actual"]
+            print(csv_metadata)
+    except FileNotFoundError:
+        min_nodes = 0
+        node_location = {}
+        actual = []
     df = pd.read_csv(_filename)
 
     temp_tril = []
@@ -115,69 +133,12 @@ if __name__ == '__main__':
 
     node = dict()
 
-    # draws = []
-    #  for value in node.values():
-    #      draws.append(create_point(value))
-    #  draw(draws)
-    node_location = {
-        "192.168.4.3": {"x": "0",
-                        "y": "0",
-                        "offset": "0",
-                        "C": "0",
-                        "R": "0"}
-        ,
-        "192.168.4.5": {"x": "2.5",
-                        "y": "0",
-                        "offset": "2.25",
-                        "C": "0",
-                        "R": "0"},
-        "192.168.4.6": {"x": "5",
-                        "y": "0",
-                        "offset": "0",
-                        "C": "0",
-                        "R": "0"},
-        "192.168.4.7": {"x": "0",
-                        "y": "2.5",
-                        "offset": "0.5",
-                        "C": "0",
-                        "R": "0"},
-        "192.168.4.8": {"x": "5",
-                        "y": "2.5",
-                        "offset": "0",
-                        "C": "0",
-                        "R": "0"},
-        "192.168.4.10": {"x": "0",
-                         "y": "5",
-                         "offset": "0",
-                         "C": "0",
-                         "R": "0"},
-        "192.168.4.11": {"x": "2.5",
-                         "y": "5",
-                         "offset": "-2.25",
-                         "C": "0",
-                         "R": "0"},
-        "192.168.4.12": {"x": "5",
-                         "y": "5",
-                         "offset": "0",
-                         "C": "0",
-                         "R": "0"}
-    }
-
-    actual = []
-
-    # df.sort_values('millis', inplace=True)
-
-    # filtered = filter_outliers_median(1.7, df)
-
     df.sort_values('millis', inplace=True)
-    df['millis'] = df.apply(lambda x: round(x.millis / 1000), axis=1)
     millis = df['millis'][0]
-    # group_millis = file.groupby['millis']
     group_by_node = df.groupby(['node'])
-
     convert = group_by_node.filter(lambda x: True)
     group_by_millis = convert.groupby(['millis'])
-    group_by_millis_filter = group_by_millis.filter(lambda x: len(pd.unique(x['node'])) >= 3)
+    group_by_millis_filter = group_by_millis.filter(lambda x: len(pd.unique(x['node'])) >= min_nodes)
 
     for name, group in group_by_millis_filter.groupby(['millis']):
         for index, row in group.iterrows():
@@ -188,8 +149,9 @@ if __name__ == '__main__':
             elif 'x' in row:
                 temp_tril.append(
                     Circle(float(row['x']), float(row['y']),
-                           rssi_to_distance(row['rssi'])))
+                           distance(row)))
                 actual.append((row['target_x'], row['target_y']))
+
         history.append(Trilateration(temp_tril.copy()))
         temp_tril = []
 
